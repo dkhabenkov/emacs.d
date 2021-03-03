e2108f20fc0c0b31b473897917ced3ca0867d38e
Denis Khabenkov <denis.khabenkov@hey.com>
2021-03-03 21:21:41 +0300

    Options: Disable the creation of backup files

--- lisp/init-options.el
+++ lisp/init-options.el
@@ -13,6 +13,6 @@
 ;; You should have received a copy of the GNU General Public License
 ;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-(add-to-list 'load-path (expand-file-name "lisp" user-emacs-directory))
+(setq make-backup-files nil)
 
-(require 'init-options)
+(provide 'init-options)
